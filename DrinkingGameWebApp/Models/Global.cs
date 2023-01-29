89e19ef7976fcd21d13623a9073512d1e11eb1dc
Jørgen <jorgenba@hiof.no>
2023-01-30 00:14:50 +0100

    Update

---
+++ DrinkingGameWebApp/Models/Global.cs
@@ -0,0 +1,8 @@
+﻿namespace DrinkingGameWebApp.Models
+{
+    public class Global
+    {
+        public static string[] NamesArray { get; set; }
+        public static int NumberOfPlayers { get; set; }
+    }
+}
