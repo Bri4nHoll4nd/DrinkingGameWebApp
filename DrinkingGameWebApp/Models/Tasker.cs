278e385e6f2a7431afd59825fb0e3c89076eaac0
Jørgen <jorgenba@hiof.no>
2023-01-30 10:39:03 +0100

    update

---
+++ DrinkingGameWebApp/Models/Tasker.cs
@@ -0,0 +1,9 @@
+﻿namespace DrinkingGame.Models
+{
+    public class Tasker
+    {
+        public string? Prompt { get; set; }
+        public string? Prompt2 { get; set; }
+        public int Type { get; set; } //0: Everyone, 1: One person, 2: Two People
+    }
+}
